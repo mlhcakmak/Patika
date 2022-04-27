7717d45bd254b8c0b13468f4d5a97ff173cf9130
sleent67 <57071691+sleent67@users.noreply.github.com>
2022-04-27 11:06:56 +0300

    1

---
+++ java 101/armstrong.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayı, rakam, toplam = 0, i = 1;
+
+        System.out.print("Bir Sayı Giriniz:");
+        sayı = input.nextInt();
+
+        while (sayı != 0) {
+            rakam = sayı % 10;
+            System.out.println(i+"ler basamağının rakamı:"+rakam);
+            i *= 10;
+            toplam += rakam;
+            sayı /= 10;
+
+        }
+        System.out.print("Basamak Sayıları Toplamı:"+toplam);
+    }
+}
